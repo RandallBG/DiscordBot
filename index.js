cd841e8a6c916f929f103d641905cd4e2375d9a7
RandallBG <Randall.bg.gosnell@gmail.com>
2021-06-23 13:53:05 -0400

    Added yt-dl and created a temp way to play music by linking a url in discord.
    It should be noted there was a problem with yt-dl that required me to modify the info.js file inside node_modules folder. This will hopefully be fixed soon hopefully.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const Youtube = require("discord-youtube-api");
 require("dotenv").config();
+const youtube = new Youtube(process.env.YOUTUBE_API);
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+
+let playList = [];
 
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("message", msg => {
-    if(msg.content === "ping") {
-        msg.reply("pong");
-    }
 });
 
 client.on('message', async message =>{
@@ -17,13 +17,27 @@ client.on('message', async message =>{
     //does not come from a guild then ignore it
     if(!message.guild) return;
 
-    if(message.content === '/join'){
+    if(message.content.includes("!play")){
         //only try to join the senders voice channel
         if(message.member.voice.channel){
             const connection = await message.member.voice.channel.join();
+            //slice off the command of !play to return a string of 
+            //the url they wish to play or the text they wish 
+            //to search for
+
+            let songUrl = message.content.slice(6);
+
+            let stream = ytdl(songUrl,{filter: 'audioonly'});
+            stream.on('error', console.error);
+            let dispatcher = await connection.play(stream);
+           
+            
         } else{
             message.reply("You need to join a voice channel first!");
         }
+    }
+    if(message.content.includes("!play")){
+        
     }
 });
 
